009466bc18023d3dbdb4117860b6b751095816ab
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-09-28 15:29:43 +0530

    [Kirti]Added:Edit the Existing Contact

--- src/Address/AddressBookMain.java
+++ src/Address/AddressBookMain.java
@@ -28,28 +28,83 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 		Scanner sc = new Scanner(System.in);
+		ArrayList<AddressBookMain> contact = new ArrayList<AddressBookMain>();
 		AddressBookMain contact1 = new AddressBookMain("Kirti", "Kumar", "krtkumar08@gmail.com",
 				"Sikar Rajasthan", "Sikar", "Rajasthan", 332315, "8837898455");
-		AddressBookMain contact2 = new AddressBookMain();
-		System.out.println("Enter the contact details of 2nd person");
-		System.out.println("Enter the first name:");
-		contact2.FIRST_NAME = sc.nextLine();
-		System.out.println("Enter the last name:");
-		contact2.LAST_NAME = sc.nextLine();
-		System.out.println("Enter the Email:");
-		contact2.EMAIL = sc.nextLine();
-		System.out.println("Enter the address:");
-		contact2.ADDRESS = sc.nextLine();
-		System.out.println("Enter the city:");
-		contact2.CITY = sc.nextLine();
-		System.out.println("Enter the state:");
-		contact2.STATE = sc.nextLine();
-		System.out.println("Enter the zip:");
-		contact2.ZIP = Integer.parseInt(sc.nextLine());
-		System.out.println("Enter the Phone Number:");
-		contact2.PHONE_NUMBER = sc.nextLine();
+		AddressBookMain contact2 = new AddressBookMain("Deepak", "Fenin", "Fenin07@gmail.com", "Jaipur Rajasthan",
+				"Jaipur", "Rajasthan", 332329, "8198028341");
+		contact.add(contact1);
+		contact.add(contact2);
+		System.out.println("The contact details of 1st person is: " + contact1);
+		System.out.println("The contact details of 2nd person is: " + contact2);
+		System.out.println("Enter the name of the candidate to edit the details: ");
+		String name = sc.nextLine();
+		int found = 0;
+		for (AddressBookMain book : contact) {
+			if (book.FIRST_NAME.equals(name)) {
+				found=1;
+				System.out.println("Select the field you want to change");
+				System.out.println(
+						"1.First Name\n2.Last Name\n3.EMail\n4.Address\n5.City\n6.State\n7.zip\n8.Phone Number");
+				int option = Integer.parseInt(sc.nextLine());
+				switch (option) {
+				case 1: {
+					System.out.println("Enter the new first name:");
+					String firstName = sc.nextLine();
+					book.FIRST_NAME = firstName;
+					break;
+				}
+				case 2: {
+					System.out.println("Enter the new last name:");
+					String lastName = sc.nextLine();
+					book.LAST_NAME = lastName;
+					break;
+				}
+				case 3: {
+					System.out.println("Enter the new EMail:");
+					String Email = sc.nextLine();
+					book.EMAIL = Email;
+					break;
+				}
+				case 4: {
+					System.out.println("Enter the new Address:");
+					String Address = sc.nextLine();
+					book.ADDRESS = Address;
+					break;
+				}
+				case 5: {
+					System.out.println("Enter the new City:");
+					String city = sc.nextLine();
+					book.CITY = city;
+					break;
+				}
+				case 6: {
+					System.out.println("Enter the new State:");
+					String state = sc.nextLine();
+					book.STATE = state;
+					break;
+				}
+				case 7: {
+					System.out.println("Enter the new zip:");
+					int zip = Integer.parseInt(sc.nextLine());
+					book.ZIP = zip;
+					break;
+				}
+				case 8: {
+					System.out.println("Enter the new phone number:");
+					String phoneNumber = sc.nextLine();
+					book.PHONE_NUMBER = phoneNumber;
+					break;
+				}
+				}
+			}
+		}
 		sc.close();
-		System.out.println("The contact details of 1st person is: "+contact1);
-		System.out.println("The contact details of 2nd person is: "+contact2);
+		if (found == 0) {
+			System.out.println("Cannot found the user");
+		} else {
+			System.out.println("The contact details of 1st person is: " + contact1);
+			System.out.println("The contact details of 2nd person is: " + contact2);
+		}
 	}
 }
